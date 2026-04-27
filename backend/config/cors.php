1f42dc8c9cff4c37d5511742834867ca3bee84da
Dedicker Remi <dedickerremi@gmail.com>
2026-04-27 10:00:17 +0200

    trimp allowed_origins_patterns to avoid empty string

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -8,11 +8,11 @@ return [
 
     'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:5173')),
 
-    'allowed_origins_patterns' => explode(',', env(
+    'allowed_origins_patterns' => array_filter(array_map('trim', explode(',', env(
         'CORS_ALLOWED_ORIGIN_PATTERNS',
         // Allow all domains for LinkedIn, Indeed and HelloWork
         '^https:\/\/([a-z0-9-]+\.)*linkedin\.com$,^https:\/\/([a-z0-9-]+\.)*indeed\.(com|fr|co\.[a-z]{2,})$,^https:\/\/([a-z0-9-]+\.)*hellowork\.com$'
-    )),
+    )))),
 
     'allowed_headers' => ['Content-Type', 'Authorization', 'Accept', 'X-Request-Token', 'X-XSRF-TOKEN'],
 
